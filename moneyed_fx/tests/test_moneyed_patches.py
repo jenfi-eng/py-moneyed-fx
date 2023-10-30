103b003e92e7188e5ae777e4f8b18a87be6f3014
Justin Louie <224840+nitsujri@users.noreply.github.com>
2023-10-30 16:04:01 +0800

    Fixing install

--- moneyed_fx/tests/test_moneyed_patches.py
+++ moneyed_fx/tests/test_moneyed_patches.py
@@ -5,4 +5,4 @@ from moneyed import Money
 class TestMoneyedPatches(TestCase):
     def test_basic(self):
         assert 1 == 1
-        assert Money(100, "USD").moneyed_fx_test()
+        assert Money(100, "USD").moneyed_fx_test() == "Moneyed FX Test: $100.00, 100."
