592f85621c5c3fc35cf2bc83d60a49a52190cdb1
Justin Louie <224840+nitsujri@users.noreply.github.com>
2023-10-30 15:38:10 +0800

    Basic project

---
+++ moneyed_fx/tests/__init__.py
@@ -0,0 +1,7 @@
+
+from django.test import TestCase
+
+
+class TestMoneyedPatches(TestCase):
+    def test_basic_test(self):
+        assert 1==1
---
+++ moneyed_fx/tests/test_moneyed_patches.py
@@ -0,0 +1,7 @@
+
+from django.test import TestCase
+
+
+class TestMoneyedPatches(TestCase):
+    def test_basic(self):
+        assert 1==1
