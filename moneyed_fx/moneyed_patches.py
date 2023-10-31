a4e77d2cf14e9020b441fa361524bac2f55e2901
Justin Louie <224840+nitsujri@users.noreply.github.com>
2023-10-31 17:10:07 +0800

    Making it a bit more readable

--- moneyed_fx/moneyed_patches.py
+++ moneyed_fx/moneyed_patches.py
@@ -20,22 +20,34 @@ def fx_to(self, to_currency, date_or_datetime=None):
     check_valid_currencies(self.currency, to_currency)
 
     if use_reverse_rates(self.currency):
-        if date_or_datetime is None or is_today(date_or_datetime):
-            rate = 1 / get_current_rate(to_currency, from_currency)
-        else:
-            rate = 1 / get_stored_rate(to_currency, from_currency, date_or_datetime)
+        rate = _reverse_rate(from_currency, to_currency, date_or_datetime)
     else:
-        if date_or_datetime is None or is_today(date_or_datetime):
-            rate = get_current_rate(from_currency, to_currency)
-        else:
-            rate = get_stored_rate(from_currency, to_currency, date_or_datetime)
+        rate = _normal_rate(from_currency, to_currency, date_or_datetime)
 
     # Convert to to_currency with rounding to sub_unit
-    fx_ed = Money(self.amount * rate, to_currency)
-    ndigits = len(str(fx_ed.currency.sub_unit)) - 1
-
-    return fx_ed.round(ndigits)
+    return _perform_fx(self, to_currency, rate)
 
 
 Money.moneyed_fx_test = moneyed_fx_test
 Money.fx_to = fx_to
+
+
+def _reverse_rate(from_currency, to_currency, date_or_datetime):
+    if date_or_datetime is None or is_today(date_or_datetime):
+        return 1 / get_current_rate(to_currency, from_currency)
+    else:
+        return 1 / get_stored_rate(to_currency, from_currency, date_or_datetime)
+
+
+def _normal_rate(from_currency, to_currency, date_or_datetime):
+    if date_or_datetime is None or is_today(date_or_datetime):
+        return get_current_rate(from_currency, to_currency)
+    else:
+        return get_stored_rate(from_currency, to_currency, date_or_datetime)
+
+
+def _perform_fx(self, to_currency, rate):
+    fx_ed = Money(self.amount * rate, to_currency)
+    ndigits = len(str(fx_ed.currency.sub_unit)) - 1
+
+    return fx_ed.round(ndigits)
