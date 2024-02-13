2c84bbba0acca3d8d4a47eadf699883130e2c8e2
Justin Louie <224840+nitsujri@users.noreply.github.com>
2024-02-13 16:27:48 +0800

    Adding db indexes for currency, forgot them

--- moneyed_fx/models.py
+++ moneyed_fx/models.py
@@ -4,5 +4,13 @@ from model_utils.models import TimeStampedModel
 
 class FxRate(TimeStampedModel):
     timestamp = models.DateTimeField(db_index=True)
-    base_currency = models.CharField(max_length=3)
+    base_currency = models.CharField(max_length=3, db_index=True)
     rates = models.JSONField()
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=["base_currency", "timestamp"],
+                name="base_currency_timestamp_idx",
+            ),
+        ]
